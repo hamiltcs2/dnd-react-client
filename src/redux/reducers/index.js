4df9df4aac2cc6604b9936cd70ef20ce63b4c7b9
hamiltcs2 <hamiltcs2@gmail.com>
2023-03-01 23:16:00 -0500

    removed bloat fields

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -6,22 +6,41 @@ const rootReducer = (state=initState, action) => {
     if (action.type === 'add_cart'){
         let newArray=state.cart;
         for (let i = 0; i < action.count; i++) {
-            newArray.push({
-                _id: action.cart._id,
-                name: action.cart.name,
-                strength: action.cart.strength,
-                dexterity: action.cart.dexterity,
-                constitution: action.cart.constitution,
-                intelligence: action.cart.intelligence,
-                wisdom: action.cart.wisdom,
-                charisma: action.cart.charisma,
-                initiative: action.cart.initiative,
-                max_hp: action.cart.max_hp,
-                armor_class: action.cart.armor_class,
-                passive_perception: action.cart.passive_perception,
-                combatantType: action.cart.combatantType,
-                __v: action.cart.__v,
-            });
+            if ('strength' in action.cart) {
+                newArray.push({
+                    _id: action.cart._id,
+                    name: action.cart.name,
+                    strength: action.cart.strength,
+                    dexterity: action.cart.dexterity,
+                    constitution: action.cart.constitution,
+                    intelligence: action.cart.intelligence,
+                    wisdom: action.cart.wisdom,
+                    charisma: action.cart.charisma,
+                    initiative: action.cart.initiative,
+                    max_hp: action.cart.max_hp,
+                    armor_class: action.cart.armor_class,
+                    passive_perception: action.cart.passive_perception,
+                    combatantType: action.cart.combatantType,
+                    __v: action.cart.__v,
+                });
+            } else {
+                newArray.push({
+                    _id: action.cart._id,
+                    name: action.cart.name,
+                    strength: "0",
+                    dexterity: action.cart.dexterity,
+                    constitution: "0",
+                    intelligence: "0",
+                    wisdom: "0",
+                    charisma: "0",
+                    initiative: action.cart.initiative,
+                    max_hp: "0",
+                    armor_class: "0",
+                    passive_perception: "0",
+                    combatantType: action.cart.combatantType,
+                    __v: action.cart.__v,
+                });
+            }
         }
         // }
         return {
